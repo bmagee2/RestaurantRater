09c6a7070ca21e5e50789dcd9ebdc6f62a6ab38f
bmagee2 <bamagee2@gmail.com>
2020-09-21 14:32:35 -0400

    added model & connection string

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;   // added for DbContext
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant // this is our Entity -- gets stored in the db
+    {
+        // COLUMNS FOR THE TABLE
+        public int RestaurantId { get; set; }   // PRIMARY KEY FOR TABLE
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public class RestaurantDbContext : DbContext    // snapshot of server/db 
+    {
+        // collection of Restaurant objects
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
