b768b569490c2c01374da2da9c2a7913c95e10ef
bmagee2 <bamagee2@gmail.com>
2020-09-21 16:19:52 -0400

    controller & index

---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,22 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // FIELD of model to pass to View()
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
+        // GET: Restaurant/Index -- list of restaurants
+        public ActionResult Index()
+        {
+            //          gives us our full table of restaurants (DbSet) & turns it into a list 
+            return View(_db.Restaurants.ToList());
+        }
+    }
+}
