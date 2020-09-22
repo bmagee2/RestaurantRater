ed39c367fbd32344d8a9d4480826ea98d8599196
bmagee2 <bamagee2@gmail.com>
2020-09-22 11:38:23 -0400

    add delete

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -38,6 +39,35 @@ namespace RestaurantRater.Controllers
             }
 
             return View(restaurant);    // return model (the filled out form) & give it back -- doesn't clear already filled out form
+        }
+
+        // GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id) // int? -- nullable id, id can now be null
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);     // user entered a bad request    
+            }
+
+            // Restaurant object    calling dbcontext Restaurants table
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();  // couldn't find restaurant
+            }
+
+            return View(restaurant);    // return restaurant matching id
+        }
+
+        // POST: Restaurant/Delete/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
